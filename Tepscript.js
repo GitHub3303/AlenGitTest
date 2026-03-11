e1d5fe8ff4c04a80a757d60a187205a7b16a6269
GitHub3303 <xiehaiqing3303@163.com>
2026-03-11 20:00:43 +0800

    Update

---
+++ Tepscript.js
@@ -0,0 +1,8 @@
+// var message:string = "Hello World"
+function printArray(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+printArray([1, 2, 3]);
+printArray(["a", "b", "c"]);
